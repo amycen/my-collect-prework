1103701dfd92425f27b6676cdbf3b77ceaeaab1f
Amy Cen <amy.cen@blackrock.com>
2019-04-16 02:34:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   new_collect = []
   i = 0
   while i < collection.length
-    new_collect << yield collection[i]
+    new_collect << yield(collection[i])
     i += 1
   end
   mew_collect
